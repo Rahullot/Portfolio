c9bf0c6cfd7a8333aa2394fc1519b003d317c5ff
Rahullot <121476719+Rahullot@users.noreply.github.com>
2025-06-17 15:51:13 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,11 +7,7 @@ export const personalData = {
   phone: '+91 93226 79329',
   address: '102, J Chandra Smurti, Satyawan Chowk, Umesh Nagar, Dombivli West, 421202 ',
   github: 'https://github.com/rahulclouddev',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
   linkedIn: 'https://www.linkedin.com/in/rahullot2105',
   twitter: 'https://x.com/Rhul92065917',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
   resume: "https://drive.google.com/file/d/1SsB3MmVkPfcJ9O54o1WTAAJKpihubruU/view?usp=sharing"
 }
