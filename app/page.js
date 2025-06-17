e64bf6b21691477b2999a3c35b7c1a4416b59520
Rahullot <121476719+Rahullot@users.noreply.github.com>
2025-06-17 13:06:44 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,47 @@
+// import { personalData } from "@/utils/data/personal-data";
+// import AboutSection from "./components/homepage/about";
+// // import Blog from "./components/homepage/blog";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
+
+// async function getData() {
+//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+
+//   const data = await res.json();
+
+//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+
+//   return filtered;
+// };
+
+// export default async function Home() {
+//   // const blogs = await getData();
+
+//   return (
+//     <div suppressHydrationWarning >
+//       <HeroSection />
+//       <AboutSection />
+//       <Experience />
+//       <Skills />
+//       <Projects />
+//       <Education />
+//       {/* <Blog blogs={blogs} /> */}
+//       <ContactSection />
+//     </div>
+//   )
+// };
+
+
+"use client";
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
@@ -8,25 +52,9 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-  // const blogs = await getData();
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -36,5 +64,5 @@ export default async function Home() {
       {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
-  )
-};
+  );
+}
